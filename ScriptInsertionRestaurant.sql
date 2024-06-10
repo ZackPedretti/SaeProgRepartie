dc9d5c9c77797ab43b496d6810d2d77fa4371543
LaheurteR <97089676+LaheurteR@users.noreply.github.com>
2024-06-10 16:35:50 +0200

    Update ScriptInsertionRestaurant.sql

--- ScriptInsertionRestaurant.sql
+++ ScriptInsertionRestaurant.sql
@@ -1,4 +1,7 @@
-CREATE TABLE IF NOT EXISTS RESTAURANT(
+DROP TABLE IF EXISTS RESERVATION;
+DROP TABLE IF EXISTS TABL;
+DROP TABLE IF EXISTS RESTAURANT;
+CREATE TABLE RESTAURANT(
     rst_idrst BIGINT,
     rst_nom VARCHAR(255),
     rst_adr varchar(255),
@@ -7,20 +10,19 @@ CREATE TABLE IF NOT EXISTS RESTAURANT(
     rst_lat DECIMAL(5,3),
 	PRIMARY KEY (rst_idrst));
     
-CREATE TABLE IF NOT EXISTS TABL (
-    tbl_idtbl BIGINT, 
+CREATE TABLE TABL (
+    tbl_idtbl BIGINT,
+    tbl_numtbl INT,
     tbl_idrst BIGINT,
 	tbl_plc INT,
     
 	PRIMARY KEY (tbl_idtbl),
     FOREIGN KEY (tbl_idrst) REFERENCES RESTAURANT (rst_idrst));
     
-CREATE TABLE IF NOT EXISTS RESERVATION (
+CREATE TABLE RESERVATION (
     rsv_idrsv BIGINT, 
 	rsv_idtbl BIGINT,
-    rsv_idrst BIGINT,
 	rsv_dat DATE, 
 	rsv_nbpers SMALLINT,
-	PRIMARY KEY (rsv_idrsv, rsv_idtbl, rsv_idrst),
-    FOREIGN KEY (rsv_idtbl) REFERENCES TABL (tbl_idtbl),
-    FOREIGN KEY (rsv_idrst) REFERENCES RESTAURANT (rst_idrst));
+	PRIMARY KEY (rsv_idrsv, rsv_idtbl),
+    FOREIGN KEY (rsv_idtbl) REFERENCES TABL (tbl_idtbl));
